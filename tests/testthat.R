badaf08c3e6e3ac112019dcde3e2f436f2d0ccdb
cawrence391 <lc0271@bristol.ac.uk>
2017-09-30 17:43:35 +0530

    package import in test

--- tests/testthat.R
+++ tests/testthat.R
@@ -1,4 +1,7 @@
 library(testthat)
 library(FARS)
+library(dplyr)
+library(magrittr)
+library(readr)
 
 test_check("FARS")
--- tests/testthat/test_load.R
+++ tests/testthat/test_load.R
@@ -1,5 +1,3 @@
-library(testthat)
-library(FARS)
 
 test_that("fars data loading is working", {
   b <- as.data.frame(fars_read_years(2013))
